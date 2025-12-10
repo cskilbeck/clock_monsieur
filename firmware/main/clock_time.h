43129ff90b3eeae5be940490ad12216f2396e723
chs <github@charlie.skilbeck.com>
2025-12-10 23:35:58 +0000

    bah

--- firmware/main/clock_time.h
+++ firmware/main/clock_time.h
@@ -8,5 +8,5 @@ void clock_update();
 //////////////////////////////////////////////////////////////////////
 
 extern timeval utc_wall_time;               // current wall time UTC
-extern int timezone_offset_seconds = 0;     // timezone offset for right now
+extern int timezone_offset_seconds;         // timezone offset for right now
 extern int timezone_next_offset_seconds;    // timezone offset for 1 second in the future
